49b9a677f5571b9518fe02404d7f38b52c318e74
jason <jason@thinkypad.attlocal.net>
2021-05-08 17:09:42 -0500

    created card wrapper component with slot

---
+++ src/components/CardWrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row class="mb-6" no-gutters>
+      <v-col md="6" offset-md="3">
+        <v-card elevation="5" class="pa-5">
+          <slot></slot>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'CardWrapper',
+};
+</script>
