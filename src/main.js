c2b773cbd8cfefbb8825125cfe8d16d3bd8ce16b
jason <jason@thinkypad.attlocal.net>
2021-05-09 14:50:22 -0500

    added login and register logic using firebase and vuex

--- src/main.js
+++ src/main.js
@@ -4,21 +4,7 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-
-import firebase from 'firebase';
-
-var firebaseConfig = {
-  apiKey: process.env.VUE_APP_apiKey,
-  authDomain: process.env.VUE_APP_authDomain,
-  projectId: process.env.VUE_APP_projectId,
-  storageBucket: process.env.VUE_APP_storageBucket,
-  messagingSenderId: process.env.VUE_APP_messagingSenderId,
-  appId: process.env.VUE_APP_appId,
-  measurementId: process.env.VUE_APP_measurementId,
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+import '@/plugins/firebase';
 
 Vue.config.productionTip = false;
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,9 +2,9 @@
   <CardWrapper>
     <v-card-title class="text-h5">Sign In</v-card-title>
     <v-form ref="form" lazy-validation>
-      <v-text-field label="email"></v-text-field>
-      <v-text-field label="password"></v-text-field>
-      <v-btn>Sign In</v-btn>
+      <v-text-field label="email" v-model="email"></v-text-field>
+      <v-text-field label="password" v-model="password"></v-text-field>
+      <v-btn @click="signIn()">Sign In</v-btn>
     </v-form>
     <hr class="mt-5" />
     <v-card-actions>
@@ -17,10 +17,23 @@
 </template>
 <script>
 import CardWrapper from '../components/CardWrapper';
+import { mapActions } from 'vuex';
 export default {
   name: 'Login',
   components: { CardWrapper },
+  data() {
+    return {
+      email: null,
+      password: null,
+      validationErrors: [],
+      firebaseError: '',
+    };
+  },
   methods: {
+    ...mapActions(['signInAction']),
+    signIn() {
+      this.signInAction({ email: this.email, password: this.password });
+    },
     redirectToRegister() {
       this.$router.push({ path: '/register' });
     },
