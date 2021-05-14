d6b033acf2c4e0ca54fcd3a439bbe234a3b24c4b
jason <jason@thinkypad.attlocal.net>
2021-05-13 20:38:51 -0500

    created light theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,21 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.green.lighten3,
+        secondary: colors.cyan.lighten5,
+        accent: colors.blueGrey.darken1,
+        error: colors.red.accent3,
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+    },
+  },
 });
