e697ea55b9ec238d26ee5ba7f66167c628c40bc5
jason <jason@thinkypad.attlocal.net>
2021-06-05 13:38:53 -0500

    separated vuex in to modules

--- src/store/modules/plant.js
+++ src/store/modules/plant.js
@@ -1,38 +1,12 @@
-import Vue from "vue";
-import Vuex from "vuex";
 import firebase from "firebase/app";
 
-Vue.use(Vuex);
-
-const initialState = () => {
-  return {
-    drawerState: false,
-    user: null,
-    error: null,
-    isUserAuthenticated: false,
-    theme: "light",
+export default {
+  state: {
     plantLogEntries: [],
     plantDetails: [],
-  };
-};
-export default new Vuex.Store({
-  state: initialState(),
+  },
+
   mutations: {
-    toggleDrawerState(state, payload) {
-      state.drawerState = payload;
-    },
-    setUser(state, payload) {
-      state.user = payload;
-    },
-    setError(state, payload) {
-      state.error = payload;
-    },
-    setUserAuth(state, payload) {
-      state.isUserAuthenticated = payload;
-    },
-    setCurrentTheme(state, data) {
-      state.theme = data;
-    },
     addPlantToLog(state, data) {
       state.plantLogEntries = [...state.plantLogEntries, data];
     },
@@ -53,40 +27,6 @@ export default new Vuex.Store({
     },
   },
   actions: {
-    signUpAction({ commit }, payload) {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(payload.email, payload.password)
-        .then((response) => {
-          commit("setUser", response.user);
-        })
-        .catch((error) => {
-          commit("setError", error.message);
-        });
-    },
-
-    signInAction({ commit }, payload) {
-      return firebase
-        .auth()
-        .signInWithEmailAndPassword(payload.email, payload.password)
-        .then((response) => {
-          commit("setUser", response.user);
-        })
-        .catch((error) => {
-          commit("setError", error.message);
-        });
-    },
-
-    async signOutAction({ commit }) {
-      try {
-        await firebase.auth().signOut();
-        commit("setUser", null);
-        commit("setUserAuth", false);
-      } catch (err) {
-        commit("setError", err.message);
-      }
-    },
-
     async addPlantToLog({ commit }, payload) {
       await firebase
         .firestore()
@@ -106,7 +46,6 @@ export default new Vuex.Store({
         .collection("users")
         .doc(firebase.auth().currentUser.uid)
         .collection("plants");
-
       firebasePlantsRef
         .get()
         .then((querySnapShot) => {
@@ -121,6 +60,7 @@ export default new Vuex.Store({
           console.warn("Error getting document:", error);
         });
     },
+
     async updatePlantEntry({ commit }, payload) {
       await firebase
         .firestore()
@@ -135,6 +75,7 @@ export default new Vuex.Store({
         });
       commit("updatePlantLogEntry", payload);
     },
+
     async deletePlantEntry({ commit }, payload) {
       await firebase
         .firestore()
@@ -146,6 +87,7 @@ export default new Vuex.Store({
 
       commit("deletePlantLogEntry", payload);
     },
+
     async getPlantDetailsByDate({ commit }, payload) {
       if (payload === null) {
         commit("addDetailsForDay", null);
@@ -189,14 +131,8 @@ export default new Vuex.Store({
       commit("addDetailsForDay", payload);
     },
   },
-  modules: {},
   getters: {
-    theme: (state) => state.theme,
-    drawerState: (state) => state.drawerState,
-    getUser: (state) => state.user,
-    isUserAuth: (state) => state.isUserAuthenticated,
-    getError: (state) => state.error,
     plantLogEntries: (state) => state.plantLogEntries,
     plantDetails: (state) => state.plantDetails,
   },
-});
+};
