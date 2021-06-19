d4ff07ced5301f38f5fb96af41bc2adf10bf0d0d
jason <jason@thinkypad.attlocal.net>
2021-06-18 20:08:02 -0500

    wip

--- src/components/PlantLogCards.vue
+++ src/components/PlantLogCards.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="cards-container">
     <template v-for="item in plantLogEntries">
-      <v-card class="ma-12" max-width="385" :key="item.id">
+      <v-card
+        class="ma-12"
+        max-width="385"
+        :key="item.id"
+        v-if="!isEditMode(item)"
+      >
         <v-card-actions class="center card-actions">
-          <v-btn v-if="!isEditMode(item)" icon @click="editItem(item)">
+          <v-btn icon @click="editItem(item)">
             <v-icon>mdi-pencil</v-icon>
-          </v-btn>
-          <v-btn v-if="isEditMode(item)" icon @click="saveItem(item)">
-            <v-icon>mdi-content-save</v-icon>
-          </v-btn>
-          <v-btn icon @click="deleteItem(item)">
-            <v-icon>mdi-delete</v-icon>
           </v-btn>
           <v-btn icon @click="goToDetails(item)">
             <v-icon>mdi-notebook</v-icon>
@@ -38,6 +37,94 @@
               {{ item.plantType }}
             </div>
             <div class="text-subtitle-1" v-if="isEditMode(item)">
+              <v-text-field
+                v-model="item.plantType"
+                :rules="rules"
+                hide-details="auto"
+                dense
+                label="Plant Type"
+                clearable
+                :filled="isEditMode(item)"
+                :readonly="!isEditMode(item)"
+                :disabled="!isEditMode(item)"
+              ></v-text-field>
+            </div>
+          </v-row>
+          <v-row justify="center" class="my-6">
+            <div class="my-4">
+              <template>
+                <vc-date-picker
+                  class="inline-block h-full"
+                  v-model="item.dateAcquired"
+                >
+                  <template v-slot="{ inputValue, togglePopover }">
+                    <div class="flex items-center">
+                      <v-btn
+                        icon
+                        @click="togglePopover()"
+                        :disabled="!isEditMode(item)"
+                        ><v-icon>mdi-calendar-month</v-icon></v-btn
+                      >
+                      <input
+                        :value="inputValue"
+                        class="
+                          bg-white
+                          text-gray-700
+                          w-full
+                          py-1
+                          px-2
+                          appearance-none
+                          border
+                          rounded-r
+                          focus:outline-none focus:border-blue-500
+                        "
+                        readonly
+                      />
+                    </div>
+                  </template>
+                </vc-date-picker>
+              </template>
+            </div>
+          </v-row>
+        </v-card-text>
+      </v-card>
+
+      <v-card
+        class="ma-12"
+        max-width="385"
+        :key="item.id"
+        v-if="isEditMode(item)"
+      >
+        <v-card-actions class="center card-actions">
+          <v-btn icon @click="saveItem(item)">
+            <v-icon>mdi-content-save</v-icon>
+          </v-btn>
+          <v-btn icon @click="deleteItem(item)">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+          <v-btn icon @click="goToDetails(item)">
+            <v-icon>mdi-notebook</v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-card-title class="center card-title my-6" v-if="!isEditMode(item)">
+          {{ item.plantName }} testing
+        </v-card-title>
+        <v-card-title>
+          <v-text-field
+            v-model="item.plantName"
+            :rules="rules"
+            hide-details="auto"
+            dense
+            label="Plant Name"
+            clearable
+            :filled="isEditMode(item)"
+            :readonly="!isEditMode(item)"
+            :disabled="!isEditMode(item)"
+          ></v-text-field>
+        </v-card-title>
+        <v-card-text>
+          <v-row justify="center" class="my-6">
+            <div class="text-subtitle-1">
               <v-text-field
                 v-model="item.plantType"
                 :rules="rules"
