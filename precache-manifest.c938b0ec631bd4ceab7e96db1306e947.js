64c552c7c4df2a2902c6875326d92b7b5434aecb
CI <CI@example.com>
2021-07-05 19:37:32 +0000

    deploy

---
+++ precache-manifest.c938b0ec631bd4ceab7e96db1306e947.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1770436ff9aa14b4766f",
+    "url": "/crazyplantperson/css/app.b4a01fce.css"
+  },
+  {
+    "revision": "be0afcfcfac3f7d696d2",
+    "url": "/crazyplantperson/css/chunk-vendors.971cf33b.css"
+  },
+  {
+    "revision": "aad4d76ea90610761bcd8c9100d27be6",
+    "url": "/crazyplantperson/img/cpp-house-dark-theme.aad4d76e.png"
+  },
+  {
+    "revision": "2223889c55e062bb85bfac6358b70cd6",
+    "url": "/crazyplantperson/img/cpp-house.2223889c.png"
+  },
+  {
+    "revision": "9cb5b5884fc8354526f15bffd88a56e2",
+    "url": "/crazyplantperson/img/plant-cartoon-img.9cb5b588.png"
+  },
+  {
+    "revision": "c487a91734aada7edd8c2e461a4f4f01",
+    "url": "/crazyplantperson/img/plant-lady-1.c487a917.png"
+  },
+  {
+    "revision": "e9ac50203bb52627ea81ea5f39e62492",
+    "url": "/crazyplantperson/img/plant-lady-2.e9ac5020.png"
+  },
+  {
+    "revision": "2ea65b12fa1c2e7a4a54cfe447ef7056",
+    "url": "/crazyplantperson/img/plant-water-lady.2ea65b12.png"
+  },
+  {
+    "revision": "00095600245387805867a8f0fa34a75a",
+    "url": "/crazyplantperson/img/sleeping-cactus.00095600.png"
+  },
+  {
+    "revision": "d2b7316d19773c2d1c58f59f482962fc",
+    "url": "/crazyplantperson/index.html"
+  },
+  {
+    "revision": "1770436ff9aa14b4766f",
+    "url": "/crazyplantperson/js/app.406d79c8.js"
+  },
+  {
+    "revision": "df785b196e41f8a5ce83",
+    "url": "/crazyplantperson/js/chunk-7b9ebe9c.aa46be8b.js"
+  },
+  {
+    "revision": "601143120dcced458cf3",
+    "url": "/crazyplantperson/js/chunk-7bc4f1a0.28acd37e.js"
+  },
+  {
+    "revision": "be0afcfcfac3f7d696d2",
+    "url": "/crazyplantperson/js/chunk-vendors.f9b12880.js"
+  },
+  {
+    "revision": "532c9c932c2a9d16ada2a477ff1ba6d7",
+    "url": "/crazyplantperson/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crazyplantperson/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/crazyplantperson/precache-manifest.c938b0ec631bd4ceab7e96db1306e947.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "crazyplantperson"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
